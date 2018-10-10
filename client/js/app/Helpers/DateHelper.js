2fae2a6d2a98482f537f8eff20c1ae442567536d
Thiago Lemos <thiago.lemos@prodap.com.br>
2018-10-10 16:59:49 -0300

    first commit

---
+++ client/js/app/Helpers/DateHelper.js
@@ -0,0 +1,20 @@
+class DateHelper {
+
+    constructor() {
+
+        throw new Error('Essa classe não pode ser instanciada');
+    }
+
+    static dataParaTexto(data) {
+
+        return data.getDate()
+            + '/' + (data.getMonth() + 1)
+            + '/' + data.getFullYear();
+
+    }
+
+    static textoParaData(texto) {
+
+        return new Date(...texto.split('-').map((item, indice) => item - indice % 2));
+    }
+}
---
+++ client/js/app/models/ListaNegociacoes.js
@@ -0,0 +1,17 @@
+class ListNegociacoes {
+
+    constructor() {
+
+        this._negociacoes = [];
+    }
+
+    adiciona(negociacao) {
+
+        this._negociacoes.push(negociacao);
+    }
+
+    get negociacoes() {
+
+        return [].concat(this._negociacoes);
+    }
+}
